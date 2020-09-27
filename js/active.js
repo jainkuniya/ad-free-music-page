9f77b8e50e7237c9b21d81f78e2d010f00c92fab
jainkuniya <jainkuniya@gmail.com>
2020-09-27 13:11:38 +0530

    add more data and images

--- js/active.js
+++ js/active.js
@@ -108,16 +108,16 @@
     }
 
     // :: 7.0 Magnific-popup Video Active Code
-    if ($.fn.magnificPopup) {
-        $('.video_btn').magnificPopup({
-            disableOn: 0,
-            type: 'iframe',
-            mainClass: 'mfp-fade',
-            removalDelay: 160,
-            preloader: true,
-            fixedContentPos: false
-        });
-    }
+    // if ($.fn.magnificPopup) {
+    //     $('.video_btn').magnificPopup({
+    //         disableOn: 0,
+    //         type: 'iframe',
+    //         mainClass: 'mfp-fade',
+    //         removalDelay: 160,
+    //         preloader: true,
+    //         fixedContentPos: false
+    //     });
+    // }
 
     $('a[href="#"]').click(function ($) {
         $.preventDefault()
